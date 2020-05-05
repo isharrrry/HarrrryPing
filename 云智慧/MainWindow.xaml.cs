df96108182e255b178b181281b4e5df190add489
Harrrry <976063930@qq.com>
2020-05-05 21:49:36 +0800

    ping

--- 云智慧/MainWindow.xaml.cs
+++ 云智慧/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace 云智慧
 {
@@ -22,6 +22,49 @@ namespace 云智慧
         public MainWindow()
         {
             InitializeComponent();
+            MessageBox.Show("正在检测网络。\nChecking Network.");
+            MessageBox.Show(RunCmd("ping", " baidu.com"));
+            Application.Current.Shutdown();
+        }
+        /// <summary>
+        /// 运行cmd命令
+        /// 不显示命令窗口
+        /// </summary>
+        /// <param name="cmdExe">指定应用程序的完整路径</param>
+        /// <param name="cmdStr">执行命令行参数</param>
+        static string RunCmd(string cmdExe, string cmdStr)
+        {
+            string result = "";
+            try
+            {
+                using (Process myPro = new Process())
+                {
+                    myPro.StartInfo.FileName = "cmd.exe";
+                    myPro.StartInfo.UseShellExecute = false;
+                    myPro.StartInfo.RedirectStandardInput = true;
+                    myPro.StartInfo.RedirectStandardOutput = true;
+                    myPro.StartInfo.RedirectStandardError = true;
+                    myPro.StartInfo.CreateNoWindow = true;
+                    myPro.Start();
+                    //如果调用程序路径中有空格时，cmd命令执行失败，可以用双引号括起来 ，在这里两个引号表示一个引号（转义）
+                    string str = string.Format(@"""{0}"" {1} {2}", cmdExe, cmdStr, "&exit");
+
+                    //myPro.StandardInput.WriteLine("cls");
+                    myPro.StandardInput.WriteLine(str);
+                    myPro.StandardInput.AutoFlush = true;
+                    //获取cmd窗口的输出信息
+                    string output = myPro.StandardOutput.ReadToEnd();
+                    myPro.WaitForExit();
+
+                    result = output;
+                }
+            }
+            catch
+            {
+
+            }
+            return result;
         }
     }
+
 }
